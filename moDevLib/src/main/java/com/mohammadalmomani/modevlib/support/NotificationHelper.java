2fb419c98537bc766997d074697ea4dcb668c992
LSS_Admin <Admin@LSS>
2024-03-14 13:17:14 +0300

    Share Library

--- moDevLib/src/main/java/com/mohammadalmomani/modevlib/support/NotificationHelper.java
+++ moDevLib/src/main/java/com/mohammadalmomani/modevlib/support/NotificationHelper.java
@@ -1,105 +1,78 @@
+// NotificationHelper.java
 package com.mohammadalmomani.modevlib.support;
-
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.os.IBinder;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.mohammadalmomani.modevlib.R;
 
 import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class NotificationHelper extends Service {
+public class NotificationHelper extends AppCompatActivity {
+
+    private final String CHANNEL_ID;
+    private final Context context;
+    private NotificationCompat.Builder builder;
 
 
-    static private NotificationChannel channel;
+    public NotificationHelper(Context context) {
+        this.context = context;
+        CHANNEL_ID = context.getString(R.string.app_name);
+        createChannel();
 
-
-    public NotificationHelper() {
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 
     private void createChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channel = new NotificationChannel(Build.BRAND.toLowerCase(Locale.ENGLISH) + "",
-                    Build.BRAND.toLowerCase(Locale.ENGLISH), NotificationManager.IMPORTANCE_HIGH);
-            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                Build.BRAND.toLowerCase(Locale.ENGLISH),
+                NotificationManager.IMPORTANCE_HIGH);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
-    public void onCreate() {
 
-        createChannel();
-        // cancel if service is  already existed
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void run() {
-                //Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
-                notifyThis(getApplicationContext(), "test", "sss", null, 20);
-
-            }
-        };
-//        timer.schedule(timerTask, 0, 3000);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+    public NotificationCompat.Builder createNotification() {
+        builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        return builder;
     }
 
     @SuppressLint("MissingPermission")
-    static public void notifyThis(Context context, String title, String message, Intent intent, int notificationId) {
-        ///////////////////////////////////
-        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_foreground);
-
-        NotificationCompat.Builder builder = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            builder = new NotificationCompat.Builder(context, channel.getId())
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentTitle(title)
-                    .setContentText(message)
-                    .setAutoCancel(true)
-                    .setLargeIcon(largeIcon)
-                    .setOngoing(false)
-                    .setPriority(NotificationCompat.PRIORITY_HIGH);
-        }
-
-        if (intent != null) {
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-            builder.setContentIntent(pendingIntent);
-        }
-
+    public void showNotification(int notificationId) {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(notificationId, builder.build());
 
-
-        ///////////////////////////////////////
     }
+
+
+//    public void notifyThis(String title, String message, Intent intent, int notificationId) {
+//        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_foreground);
+//
+//        NotificationCompat.Builder builder = null;
+//        builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle(title)
+//                .setContentText(message)
+//                .setAutoCancel(true)
+//                .setLargeIcon(largeIcon)
+//                .setOngoing(false)
+//                .setPriority(NotificationCompat.PRIORITY_HIGH);
+//
+//        if (intent != null) {
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+//            builder.setContentIntent(pendingIntent);
+//        }
+//
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+//        notificationManager.notify(notificationId, builder.build());
+//    }
 }
