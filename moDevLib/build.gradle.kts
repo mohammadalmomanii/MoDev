c2779f1311f26c50040de99de89152f243a04d84
LSS_Admin <Admin@LSS>
2024-09-04 11:08:28 +0300

    solve button in class MessageDialogFragment make it able to get null interface 1.2.5

--- moDevLib/build.gradle.kts
+++ moDevLib/build.gradle.kts
@@ -40,20 +40,20 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 
     /*********************|Get Image via url|*********************/
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    /*********************|View Model|*********************/
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+//    /*********************|View Model|*********************/
+//    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 
 //    /*********************|generate excl file|*********************/
 //    implementation("org.apache.poi:poi:5.2.3")
@@ -71,7 +71,7 @@ publishing {
 
             groupId = "com.mohammadalmomani.modevlib"
             artifactId = "modevlib"
-            version = "1.2.3"
+            version = "1.2.5"
 
             // Optional: Customize other publication settings if needed
             afterEvaluate {
