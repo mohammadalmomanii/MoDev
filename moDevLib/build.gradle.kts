bc81fd0cc63d5e0741fc7b28a29bdcf272f58634
LSS_Admin <Admin@LSS>
2024-09-23 16:07:41 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	moDevLib/src/main/java/com/mohammadalmomani/modevlib/support/AppHelper.java

--- moDevLib/build.gradle.kts
+++ moDevLib/build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
 
             groupId = "com.mohammadalmomani.modevlib"
             artifactId = "modevlib"
-            version = "1.2.6"
+            version = "1.2.7"
 
             // Optional: Customize other publication settings if needed
             afterEvaluate {
