81fdbba42292eaa2b336b9ffa2b731a97533ebae
LSS_Admin <Admin@LSS>
2025-07-27 11:24:00 +0300

    update SwipeRefresh -> swipeRefresh compiler was think SwipeRefresh is class but it was package 1.5.61

--- moDevLib/src/main/java/com/mohammadalmomani/modevlib/swipeRefresh/SwipeRefreshViewBehavior.java
+++ moDevLib/src/main/java/com/mohammadalmomani/modevlib/swipeRefresh/SwipeRefreshViewBehavior.java
@@ -4,44 +4,41 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.appbar.AppBarLayout;
 
-public class SwipeRefreshViewBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
+public class SwipeRefreshViewBehavior extends CoordinatorLayout.Behavior<View> {
 
     public SwipeRefreshViewBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, LinearLayout child, View dependency) {
-        // This behavior depends on the SwipeRefreshView
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
         return dependency instanceof SwipeRefreshView;
     }
+
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout child, View dependency) {
-        if (dependency instanceof SwipeRefreshView) {
-            SwipeRefreshView swipeRefreshView = (SwipeRefreshView) dependency;
+    public boolean onDependentViewChanged(CoordinatorLayout parent, final View child, View dependency) {
+        if (dependency instanceof SwipeRefreshView swipeRefreshView) {
             swipeRefreshView.getAppBar().addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
                 @Override
                 public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                     if (Math.abs(verticalOffset) == 1) {
                         // AppBar fully expanded
                         int offset = swipeRefreshView.getHeight();
-                        child.animate().translationY(offset).setDuration(300).start(); // Smooth animation
-                         Log.d("SAFDSAF", "AppBar is fully expanded");
+                        child.animate().translationY((float) offset).setDuration(300L).start();
+                        Log.d("SwipeRefreshBehavior", "AppBar is fully expanded");
                     } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
                         // AppBar fully collapsed
-                        child.animate().translationY(0).setDuration(300).start(); // Smooth animation
-                        Log.d("SAFDSAF", "AppBar is fully collapsed");
+                        child.animate().translationY(0f).setDuration(300L).start();
+                        Log.d("SwipeRefreshBehavior", "AppBar is fully collapsed");
                     }
                 }
             });
-
-
             return true;
         }
         return false;
